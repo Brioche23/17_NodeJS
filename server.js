e7c7b65e0fbf545b1d359a8177ed62e9fea92430
Brioche23 <alessio.brioche@gmail.com>
2021-11-17 11:33:45 +0100

    added sketch

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("up and running");
+//  Load express
+let express = require("express");
+//  Acrivate express
+let app = express();
+//  Defined port
+const port = 8000;
+//  Defined server and connection
+let server = app.listen(port);
+console.log("Server is running on https://localhost:" + port);
+
+app.use(express.static("public"));
