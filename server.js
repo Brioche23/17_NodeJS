21d246a589efdf36ee5229996f5507e79ed201cc
Brioche23 <alessio.brioche@gmail.com>
2021-11-17 13:04:41 +0100

    added heroku compatibility

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ console.log("up and running");
 let express = require("express");
 //  Acrivate express
 let app = express();
-//  Defined port
-const port = 8000;
+//  Defined port (for localhost)
+// const port = 8000;
+//  Port created by heroku or Locally
+const port = process.env.PORT || 8000;
+
 //  Defined server and connection
 let server = app.listen(port);
 console.log("Server is running on https://localhost:" + port);
